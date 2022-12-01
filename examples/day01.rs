74afc3f3cdea181c4cd18465a23d06280b5f317e
Johan Følsgaard <johan@foelsgaard.eu>
2022-12-01 06:35:20 +0100

    Implement day01

---
+++ examples/day01.rs
@@ -0,0 +1,23 @@
+use aoc2022::get_input;
+
+fn main() {
+    let contents = get_input();
+
+    let mut totals = [0, 0, 0, 0];
+    let mut total = 0;
+
+    for line in contents.lines() {
+        if line.is_empty() {
+            totals[0] = total;
+            totals.sort();
+            total = 0;
+            continue;
+        }
+        total += line.parse::<usize>().unwrap();
+    }
+    totals[0] = total;
+    totals.sort();
+
+    println!("{}", totals[3]);
+    println!("{}", totals[1..].iter().sum::<usize>());
+}

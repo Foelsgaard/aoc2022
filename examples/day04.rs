596eab7e7bb40798ec11c2f6c1b5da8466f93dc8
Johan Følsgaard <johan@foelsgaard.eu>
2022-12-04 06:20:39 +0100

    Implement day04

---
+++ examples/day04.rs
@@ -0,0 +1,26 @@
+use aoc2022::get_input;
+
+fn main() {
+    let contents = get_input();
+
+    let mut score1 = 0;
+    let mut score2 = 0;
+
+    for line in contents.lines() {
+        let mut endpoints = line
+            .split(',')
+            .flat_map(|x| x.split('-'))
+            .flat_map(|x| x.parse::<usize>().ok());
+
+        let a = endpoints.next().unwrap();
+        let b = endpoints.next().unwrap();
+        let c = endpoints.next().unwrap();
+        let d = endpoints.next().unwrap();
+
+        score1 += (a <= c && b >= d || c <= a && d >= b) as usize;
+        score2 += (b >= c && a <= d) as usize;
+    }
+
+    println!("{}", score1);
+    println!("{}", score2);
+}

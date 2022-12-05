95652091e83da2fd90147c5d13930ff31efaeba8
Johan Følsgaard <johan@foelsgaard.eu>
2022-12-05 10:09:31 +0100

    Fix minor clippy warning

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::io::Read;
 use std::{env, fs};
 
 pub fn get_input() -> String {
-    let path = env::args().skip(1).next().unwrap();
+    let path = env::args().nth(1).unwrap();
     let mut file = fs::File::open(path).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();

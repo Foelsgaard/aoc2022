4306addc250425c9d78afe937bf71bd44a216b25
Johan Følsgaard <johan@foelsgaard.eu>
2022-12-06 07:53:39 +0100

    day06: Optimize main loop
    Use a running tally of uniques instead of iterating over the entire
    checklist on every character.

--- examples/day06.rs
+++ examples/day06.rs
@@ -10,22 +10,31 @@ fn main() {
 fn solve(contents: &str, marker_size: usize) -> usize {
     let bytes = contents.as_bytes();
 
-    let mut i = 0;
-    let mut checklist = [false; 256];
-    for window in bytes.windows(marker_size) {
-        for b in window {
-            checklist[*b as usize] = true;
-        }
-
-        let n = checklist.iter().filter(|x| **x).count();
-
-        if n == marker_size {
-            break;
-        }
-
-        checklist.fill(false);
-        i += 1;
+    let mut n = 0;
+    let mut checklist = [0_u8; 256];
+    for b in bytes.iter().take(marker_size) {
+        let check = &mut checklist[*b as usize];
+        *check += 1;
+        n += (*check == 1) as u8;
     }
 
-    i + marker_size
+    let mut i: usize = 0;
+    let mut j: usize = marker_size;
+    while n as usize != marker_size {
+        let a = bytes[i];
+        let b = bytes[j];
+
+        let check_a = &mut checklist[a as usize];
+        *check_a -= 1;
+        n -= (*check_a == 0) as u8;
+
+        let check_b = &mut checklist[b as usize];
+        *check_b += 1;
+        n += (*check_b == 1) as u8;
+
+        i += 1;
+        j += 1;
+    }
+
+    j
 }

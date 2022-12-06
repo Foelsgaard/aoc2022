36c749dd9fb015dc6f485d7373ab46d35891a687
Johan Følsgaard <johan@foelsgaard.eu>
2022-12-06 10:17:50 +0100

    Add run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+./target/release/examples/day01 input/day01
+./target/release/examples/day02 input/day02
+./target/release/examples/day03 input/day03
+./target/release/examples/day04 input/day04
+./target/release/examples/day05 input/day05
+./target/release/examples/day06 input/day06

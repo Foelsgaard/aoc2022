2d5c5c735f2c0d4d7afde28980215f000ac1f0cc
Johan Følsgaard <johan@foelsgaard.eu>
2022-12-08 22:40:37 +0100

    day08: Optimize solution

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -15,13 +15,15 @@ fn main() {
 
 fn solve(contents: &str) -> (usize, usize) {
     let mut grid = [0_u8; 0x10000];
+    let mut visible = [false; 0x10000];
+    let mut scenic = [1; 0x10000];
 
     let grid_width = contents.lines().next().unwrap().len();
 
     let mut n = 0;
     for ch in contents.chars() {
         match ch {
-            '\n' => {},
+            '\n' => {}
             _ => {
                 grid[n] = (ch as u8) - 48;
                 n += 1;
@@ -29,91 +31,90 @@ fn solve(contents: &str) -> (usize, usize) {
         }
     }
 
-    let mut score1 = 0;
-    let mut score2 = 0;
-    for i in 0..n {
-        let h = grid[i];
-        let x0 = i % grid_width;
-        let y0 = i / grid_width;
+    let mut calc = DistCalc::new();
 
-        let mut v = [
-            grid_width - x0 - 1,
-            x0,
-            grid_width - y0 - 1,
-            y0
-        ];
+    for j in 1..grid_width - 1 {
+        let a = j * grid_width;
+        let b = a + grid_width;
+        let row_iter = grid[a..b].iter().copied();
 
-        let mut visible = 4;
-    
-        let mut d = 1;
-        for x in x0 + 1..grid_width {
-            let j = y0 * grid_width + x;
+        let col_iter = (0..grid_width)
+            .into_iter()
+            .map(|i| grid[i * grid_width + j]);
 
-            if h <= grid[j] {
-                v[0] = d;
-                visible -= 1;
-                break;
-            }
-
-            d += 1;
-        }
-
-        d = 1;
-        for x in (0..x0).rev() {
-            let j = y0 * grid_width + x;
-
-            if h <= grid[j] {
-                v[1] = d;
-                visible -= 1;
-                break;
-            }
-
-            d += 1;
-        }
-
-        d = 1;
-        for y in y0 + 1..grid_width {
-            let j = y * grid_width + x0;
-
-            if h <= grid[j] {
-                v[2] = d;
-                visible -= 1;
-                break;
-            }
-
-            d += 1;
-        }
-
-        d = 1;
-        for y in (0..y0).rev() {
-            let j = y * grid_width + x0;
-
-            if h <= grid[j] {
-                v[3] = d;
-                visible -= 1;
-                break;
-            }
-
-            d += 1;
-        }
-
-        let s = v.iter().product();
-
-        score1 += (visible > 0) as usize;
-        score2 = score2.max(s);
+        calc.distances(row_iter, |k, d, v| {
+            let i = a + k;
+            visible[i] |= v;
+            scenic[i] *= d;
+        });
+        calc.distances(col_iter, |k, d, v| {
+            let i = k * grid_width + j;
+            visible[i] |= v;
+            scenic[i] *= d;
+        });
     }
 
-    (score1, score2)
+    let score1 = visible[..n].iter().filter(|b| **b).count() + 4;
+    let score2 = scenic[..n].iter().max().unwrap();
+
+    (score1, *score2 as usize)
+}
+
+struct DistCalc {
+    istack: [usize; 0x80],
+    hstack: [u8; 0x80],
+}
+
+impl DistCalc {
+    fn new() -> Self {
+        Self {
+            istack: [0; 0x80],
+            hstack: [0; 0x80],
+        }
+    }
+    fn distances<I, F>(&mut self, line: I, mut f: F)
+    where
+        I: IntoIterator<Item = u8>,
+        F: FnMut(usize, usize, bool),
+    {
+        let istack = &mut self.istack;
+        let hstack = &mut self.hstack;
+
+        let mut n = 0;
+        let mut i = 0;
+        for h in line {
+            while n > 0 && hstack[n - 1] < h {
+                f(istack[n - 1], i - istack[n - 1], false);
+                n -= 1;
+            }
+            let d = if n > 0 { i - istack[n - 1] } else { i };
+            f(i, d, n == 0);
+
+            while n > 0 && hstack[n - 1] == h {
+                f(istack[n - 1], i - istack[n - 1], false);
+                n -= 1;
+            }
+            istack[n] = i;
+            hstack[n] = h;
+            n += 1;
+            i += 1;
+        }
+
+        while n > 0 {
+            f(istack[n - 1], i - istack[n - 1] - 1, true);
+            n -= 1;
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn test_solution() {
         let contents = include_str!("../../input/day08");
-        
+
         let (a, b) = solve(contents);
 
         assert_eq!(a, 1711);
@@ -130,8 +131,6 @@ mod benches {
     fn bench_solution(bencher: &mut Bencher) {
         let contents = include_str!("../../input/day08");
 
-        bencher.iter(|| {
-            solve(contents)
-        });
+        bencher.iter(|| solve(contents));
     }
 }

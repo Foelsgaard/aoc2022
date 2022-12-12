b6e1217922293d5eaea495cdad826fbc92f5559a
Johan Følsgaard <johan@foelsgaard.eu>
2022-12-12 12:50:39 +0100

    day12: Optimize solution

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -13,11 +13,11 @@ fn main() {
     println!("12b: {}", b);
 }
 
-const GRID_SIZE: usize = 0x50;
+const GRID_SIZE: usize = 0x1400;
 
 fn solve(contents: &str) -> (usize, usize) {
-    let mut grid = [0_u8; GRID_SIZE * GRID_SIZE];
-    let mut pot_starts = [0; GRID_SIZE * GRID_SIZE];
+    let mut grid = [0_u8; GRID_SIZE];
+    let mut pot_starts = [0; GRID_SIZE];
     let mut pot_start_len = 0;
 
     let mut starti = 0;
@@ -26,19 +26,20 @@ fn solve(contents: &str) -> (usize, usize) {
     let width = contents.lines().next().unwrap().len();
     let height = contents.lines().count();
 
-    contents.lines()
+    contents
+        .lines()
         .flat_map(|line| line.as_bytes())
         .enumerate()
         .for_each(|(i, b)| {
-            if *b == 'S' as u8 {
+            if *b == b'S' {
                 starti = i;
-                grid[i] = 'a' as u8;
+                grid[i] = b'a';
                 pot_starts[pot_start_len] = i;
                 pot_start_len += 1;
-            } else if *b == 'E' as u8 {
+            } else if *b == b'E' {
                 endi = i;
-                grid[i] = 'z' as u8;
-            } else if *b == 'a' as u8 {
+                grid[i] = b'z';
+            } else if *b == b'a' {
                 grid[i] = *b;
                 pot_starts[pot_start_len] = i;
                 pot_start_len += 1;
@@ -61,6 +62,89 @@ fn solve(contents: &str) -> (usize, usize) {
     (score1, score2)
 }
 
+fn lookup_insert(set: &mut [usize], key: usize, new: usize) -> usize {
+    use core::num::Wrapping;
+    let mut i = Wrapping((key & 0xFFFF) as u16);
+
+    while set[i.0 as usize] != usize::MAX && set[i.0 as usize] != key {
+        i += 1;
+    }
+
+    let old = set[i.0 as usize];
+    set[i.0 as usize] = new;
+    old
+}
+
+const HEAP_SIZE: usize = 0x200;
+struct MinHeap<K, V> {
+    elems: [(K, V); HEAP_SIZE],
+    len: usize,
+}
+
+impl<K: Ord + Default + Copy + core::fmt::Debug, V: Default + Copy + core::fmt::Debug>
+    MinHeap<K, V>
+{
+    fn new() -> Self {
+        Self {
+            elems: [(K::default(), V::default()); HEAP_SIZE],
+            len: 0,
+        }
+    }
+
+    fn insert(&mut self, key: K, value: V) {
+        let mut index = self.len;
+        self.elems[index] = (key, value);
+        self.len += 1;
+
+        while index > 0 {
+            let parent = (index - 1) >> 1;
+            if self.elems[parent].0 < self.elems[index].0 {
+                break;
+            }
+
+            self.elems.swap(parent, index);
+            index = parent;
+        }
+    }
+
+    fn pop(&mut self) -> Option<(K, V)> {
+        use core::mem;
+        if self.len == 0 {
+            return None;
+        }
+
+        let mut index = 0;
+
+        let popped = mem::take(&mut self.elems[index]);
+        self.len -= 1;
+        self.elems.swap(index, self.len);
+
+        loop {
+            let left = (index << 1) + 1;
+            let right = (index << 1) + 2;
+
+            let mut min = if left < self.len && self.elems[left].0 < self.elems[index].0 {
+                left
+            } else {
+                index
+            };
+
+            if right < self.len && self.elems[right].0 < self.elems[min].0 {
+                min = right;
+            };
+
+            if min == index {
+                break;
+            }
+
+            self.elems.swap(index, min);
+            index = min;
+        }
+
+        Some(popped)
+    }
+}
+
 fn least_steps(grid: &[u8], width: usize, height: usize, starti: usize, endi: usize) -> usize {
     let heu = |x0, y0, x1, y1| {
         let dx: isize = x0 - x1;
@@ -73,28 +157,32 @@ fn least_steps(grid: &[u8], width: usize, height: usize, starti: usize, endi: us
     let x1 = (endi % width) as isize;
     let y1 = (endi / width) as isize;
 
-    let mut came_from = [usize::MAX; GRID_SIZE * GRID_SIZE];
-    let mut g_score = [usize::MAX; GRID_SIZE * GRID_SIZE];
-    let mut f_score = [usize::MAX; GRID_SIZE * GRID_SIZE];
+    let mut came_from = [usize::MAX; GRID_SIZE];
+    let mut g_score = [usize::MAX; GRID_SIZE];
+    let mut f_score = [usize::MAX; GRID_SIZE];
 
     g_score[starti] = 0;
     f_score[starti] = heu(x0, y0, x1, y1);
 
-    let mut queue = [0; GRID_SIZE * GRID_SIZE];
-    let mut qlen = 1;
+    let mut set = [usize::MAX; 0x2000];
+    let mut queue = MinHeap::new();
 
-    queue[0] = starti;
+    queue.insert(f_score[starti], starti);
+    lookup_insert(&mut set, starti, starti);
 
     let mut current;
 
     loop {
-        queue[..qlen].sort_by(|i, j| f_score[*j].cmp(&f_score[*i]));
-
-        if qlen == 0 {
-            return usize::MAX;
+        match queue.pop() {
+            Some((_, v)) => {
+                current = v;
+            }
+            None => {
+                return usize::MAX;
+            }
         }
-        current = queue[qlen - 1];
-        qlen -= 1;
+
+        lookup_insert(&mut set, current, usize::MAX);
 
         if current == endi {
             break;
@@ -125,13 +213,11 @@ fn least_steps(grid: &[u8], width: usize, height: usize, starti: usize, endi: us
                 g_score[next] = tentative_g_score;
                 f_score[next] = tentative_g_score + heu(nx, ny, x1, y1);
 
-                for i in &queue[..qlen] {
-                    if *i == next {
-                        continue 'n_loop;
-                    }
+                if lookup_insert(&mut set, next, next) == next {
+                    continue 'n_loop;
                 }
-                queue[qlen] = next;
-                qlen += 1;
+
+                queue.insert(f_score[next], next);
             }
         }
     }
@@ -144,12 +230,31 @@ fn least_steps(grid: &[u8], width: usize, height: usize, starti: usize, endi: us
     }
 
     steps
-
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_min_heap() {
+        let mut heap = MinHeap::new();
+
+        for k in [1243, 2, 347, 9, 0, 4, 1, 243, 58234] {
+            heap.insert(k, 0);
+        }
+
+        assert_eq!(heap.pop(), Some((0, 0)));
+        assert_eq!(heap.pop(), Some((1, 0)));
+        assert_eq!(heap.pop(), Some((2, 0)));
+        assert_eq!(heap.pop(), Some((4, 0)));
+        assert_eq!(heap.pop(), Some((9, 0)));
+        assert_eq!(heap.pop(), Some((243, 0)));
+        assert_eq!(heap.pop(), Some((347, 0)));
+        assert_eq!(heap.pop(), Some((1243, 0)));
+        assert_eq!(heap.pop(), Some((58234, 0)));
+        assert_eq!(heap.pop(), None);
+    }
 
     #[test]
     fn test_solution() {

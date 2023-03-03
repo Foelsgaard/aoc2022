cb8209753506b609328a79a3ef8855bf9ff21d4d
Johan Følsgaard <johan@foelsgaard.eu>
2023-03-03 13:12:36 +0100

    Optimize day01

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -2,35 +2,50 @@
 #[cfg(test)]
 extern crate test;
 
-use aoc2022::get_input;
+use aoc2022::read_input;
 
 fn main() {
-    let contents = get_input();
+    let mut buf = [0; 0x10000];
+    let contents = read_input(&mut buf);
 
-    let (a, b) = solve(&contents);
+    let (a, b) = solve(contents);
 
-    println!("01a: {}", a);
-    println!("01b: {}", b);
+    println!("01a: {a}");
+    println!("01b: {b}");
 }
 
-fn solve(contents: &str) -> (usize, usize) {
-    let mut totals = [0, 0, 0, 0];
-    let mut total = 0;
+#[inline(never)]
+fn solve(contents: &str) -> (u32, u32) {
+    let bytes = contents.as_bytes();
+    let n = bytes.len();
 
-    for line in contents.lines() {
-        if line.is_empty() {
-            totals[0] = total;
-            totals.sort_unstable();
-            total = 0;
-            continue;
+    let mut totals = [0, 0, 0, 0];
+
+    let mut i = 0;
+    let mut x = 0;
+
+    while i < n {
+        let b = bytes[i];
+        if b == b'\n' {
+            if x == 0 {
+                totals.sort_unstable();
+                totals[0] = 0;
+            } else {
+                totals[0] += x;
+                x = 0;
+            }
+        } else {
+            x *= 10;
+            x += (b - 48) as u32;
         }
-        total += line.parse::<usize>().unwrap();
+
+        i += 1;
     }
-    totals[0] = total;
+
     totals.sort_unstable();
 
     let a = totals[3];
-    let b = totals[1..].iter().sum();
+    let b = totals[1] + totals[2] + totals[3];
 
     (a, b)
 }

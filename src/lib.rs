a5b6d7ce59450ce5206a3c0bcbbc0cd02a6a662c
Johan Følsgaard <johan@foelsgaard.eu>
2023-03-03 13:12:30 +0100

    Add read_input lib function

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@
 extern crate test;
 
 use std::io::Read;
-use std::{env, fs};
+use std::{env, fs, str};
 
 pub fn get_input() -> String {
     let path = env::args().nth(1).unwrap();
@@ -13,4 +13,12 @@ pub fn get_input() -> String {
     file.read_to_string(&mut contents).unwrap();
 
     contents
+}
+
+pub fn read_input(buf: &mut [u8]) -> &str {
+    let path = env::args().nth(1).unwrap();
+    let mut file = fs::File::open(path).unwrap();
+    let n = file.read(buf).unwrap();
+
+    str::from_utf8(&buf[..n]).unwrap()
 }
